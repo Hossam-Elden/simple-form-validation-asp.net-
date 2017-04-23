9c0ce11432d74d7d443438a7005fc3bb608b14fd
Hosam elden <Hosam elden>
2017-04-23 22:00:08 +0200

    asp dot net form

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int Hour = DateTime.Now.Hour;
+            ViewBag.Greeting = Hour < 12 ? "Good Morning" :  "Good A fater noon";
+            return  View();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm( GuestResponse   guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("thanks", guestResponse);
+            }
+            else
+            {
+                //there is error 
+                return View();
+            }
+        }
+    }
+}
